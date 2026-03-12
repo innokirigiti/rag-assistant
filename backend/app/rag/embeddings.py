f7e5dc31b1adfff419a548c463201495d60397dc
innokirigiti <innocentiousabel@gmail.com>
2026-03-12 14:21:14 +0100

    Implemented chunking (roughly)

--- backend/app/rag/embeddings.py
+++ backend/app/rag/embeddings.py
@@ -15,16 +15,24 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from app.rag.chunking import get_text_splitter
 from app.rag.vectorstore import get_vectorstore
 
-# PDF → chunks → embeddings → DB
+# PDF → Page(Document) → chunks → embeddings → DB
+# PDF → PyMuPDFLoader → pages → RecursiveCharacterTextSplitter → chunks → HuggingFaceEmbeddings → pgvector → PostgreSQL
 
 def embed_pdf(file_path: str):
 
     # Load the pdf as a single doc from PyMuPDF 
     loader = PyMuPDFLoader(file_path, mode="single")
-    docs = loader.load()
+    doc = loader.load()
+    doc_content = doc[0].page_content
+    doc_metadata = doc[0].metadata
+
+    # Take the entire pdf text & chunk it
+    text_splitter = get_text_splitter()
+    chunks = text_splitter.create_documents([doc_content], [doc_metadata])
 
     # Load the vectorstore and add the docs
     vector_store = get_vectorstore()
-    vector_store.add_documents(docs)
+    vector_store.add_documents(chunks)
 
-    return {"status":"added", }
+    return {"status":"chunks added to the db"}
+
