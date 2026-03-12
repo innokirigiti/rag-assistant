dfa437e7e55f49697505fbd40004aa546db9a690
innokirigiti <innocentiousabel@gmail.com>
2026-03-12 18:10:13 +0100

    Added starter code for file upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,11 @@ POST /upload → accepts UploadFile (one PDF) and calls ingestion service
 POST /ask → accepts JSON body with question and calls QA service
 '''
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from pathlib import Path
+import shutil
+
+# App modules
 from app.rag.embeddings import embed_pdf
 from app.models import Question
 
@@ -26,12 +30,37 @@ def greeting():
     return {"Hello from RAG assistant AI"} 
 
 # TODO: update to accept a pdf file path from the UI later
-@app.get("/embed")
-def embed():
-    # Hard coded test file path
-    path = "../../data/uploads/test.pdf"
-    return embed_pdf(path)
+# @app.get("/embed")
+# def embed():
+#     # Hard coded test file path
+#     path = "../../data/uploads/test.pdf"
+#     return embed_pdf(path)
 
+@app.post("/uploadpdf")
+async def upload_pdf(file: UploadFile = File(...)):
+
+    # Define the upload directory
+    UPLOAD_DIR = Path("../../data/uploads")
+    UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+    # Ensure it is a PDF
+    if file.content_type != "application/pdf":
+        return {"error": "Only PDF files are allowed"}
+
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="Filename missing")
+
+    save_path = UPLOAD_DIR / file.filename
+
+    # Save the file
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "status": "uploaded",
+        "filename": file.filename,
+        "path": str(save_path)
+    }
 
 # An endpoint for asking a question
 @app.post("/question")
--- ui/app.py
+++ ui/app.py
@@ -1,10 +1,52 @@
 import streamlit as st
-import pandas as pd
-    
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_BASE_URL = os.getenv("API_BASE_URL")
+if not API_BASE_URL:
+    raise ValueError("API_BASE_URL not found in .env")
+
 st.write("""
-# My first app
-Hello *world!*
+# Welcome to DocAI - A RAG AI agent
 """)
     
-# df = pd.read_csv("my_data.csv")
-# st.line_chart(df)
+    
+
+st.title("Upload a PDF")
+
+uploaded_file = st.file_uploader(
+    label="Choose a PDF file",
+    type=["pdf"]
+)
+
+def upload_file(file):
+
+    
+    UPLOAD_URL = f"{API_BASE_URL}/uploadpdf"
+
+    with st.spinner("Uploading file..."):
+
+        files = {
+            "file": (file.name, file.getvalue(), "application/pdf")
+        }
+
+        response = requests.post(UPLOAD_URL, files=files)
+
+        if response.status_code == 200:
+            st.success("File uploaded successfully!")
+            st.json(response.json())
+        else:
+            st.error("Upload failed")
+
+
+if uploaded_file is not None:
+
+    st.write(f"Selected file: **{uploaded_file.name}**")
+
+    st.button(
+        "Upload",
+        on_click=upload_file,
+        args=(uploaded_file,)
+    )
